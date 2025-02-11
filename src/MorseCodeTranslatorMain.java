e51c228dd6a5a57d88254839501fe98de9b3a3bc
linneaesqui <linneaesqui@gmail.com>
2025-02-11 17:39:26 +0700

    Add try/catch to int choice, add input validation for empty String and unrecognizable input

--- src/MorseCodeTranslatorMain.java
+++ src/MorseCodeTranslatorMain.java
@@ -13,28 +13,61 @@ public class MorseCodeTranslatorMain {
             System.out.println("What do you want to do?" +
                     "\n1. Translate English to Morse" +
                     "\n2. Translate Morse to English" +
-                    "\n3. Write out the Morse alphabet" +
+                    "\n3. Print Morse alphabet" +
                     "\n4. Exit program");
-            int choice = Integer.parseInt(scan.nextLine());
+            //Vi kastar ett NumberFormatException för att fånga upp om användaren skriver in nåt annat än siffror.
+            //Ogiltiga siffror fångas upp i default.
+            try {
+                int choice = Integer.parseInt(scan.nextLine());
 
-            switch (choice) {
-                case 1:
-                    System.out.println("Write the text you want to translate to Morse: (Only English letters!)");
-                    System.out.println(myTranslator.englishToMorse(scan.nextLine()));
-                    break;
-                case 2:
-                    System.out.println("Write the Morse code you want to translate to English:");
-                    System.out.println(myTranslator.morseToEnglish(scan.nextLine()));
-                    break;
-                case 3:
-                    System.out.println(myTranslator.printMorseAlphabet());
-                    break;
-                case 4:
-                    System.out.println("Program closing down...");
-                    isRunning = false;
-                    break;
-                default:
-                    System.out.println("Invalid input, please pick a number between 1 and 4!");
+                switch (choice) {
+                    case 1:
+                        System.out.println("Write the message you want to translate to Morse (only English letters):");
+                        while (true) {
+                            String input = scan.nextLine();
+                            if (input.isEmpty()) {
+                                System.out.println("Empty string, please try again!");
+                            } else if (myTranslator.englishToMorse(input).matches("^\\?([ ?]*\\?)?$")) {
+                                System.out.println("No matches found!");
+                                break;
+                            } else {
+                                System.out.println("Your text was: \n" + input);
+                                System.out.println("The translation is: \n" + myTranslator.englishToMorse(input));
+                                break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Write the message you want to translate to English:");
+
+                        while (true) {
+                            String input = scan.nextLine();
+                            if (myTranslator.morseToEnglish(input).isEmpty()) {
+                                System.out.println("Empty string, please try again!");
+                            } else if (myTranslator.morseToEnglish(input).matches("^\\?([ ?]*\\?)?$")) {
+                                System.out.println("No matches found!");
+                                break;
+                            } else {
+                                System.out.println("Your Morse Code message was:\n" + input);
+                                System.out.println("The English translation is:\n" + myTranslator.morseToEnglish(input));
+                                break;
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println(myTranslator.printMorseAlphabet());
+                        break;
+                    case 4:
+                        System.out.println("Program closing down...");
+                        isRunning = false;
+                        break;
+                    default:
+                        System.out.println("Invalid input, please pick a number between 1 and 4!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number!");
+            } catch (Exception e) {
+                System.out.println("Something went wrong, please try again!");
             }
         }
     }
